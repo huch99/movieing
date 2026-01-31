232273489fbbc0ab5e59ba9607d862c64ff5dc76
huch99 <huichan58@naver.com>
2026-01-31 17:55:28 +0900

    MainHome 생성

---
+++ frontEnd/project/src/pages/enduser/home/MainHome.tsx
@@ -0,0 +1,12 @@
+import Header from "../../../components/layouts/enduser/Header";
+import './css/MainHome.css';
+
+const MainHome = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default MainHome;
